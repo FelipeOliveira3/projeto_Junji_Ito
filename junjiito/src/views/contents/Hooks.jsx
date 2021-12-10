bdf25f4fcd6b8bd5f398accc689da4cf27d9bf87
FelipeOliveira3 <felipe.soliveira357@gmail.com>
2021-12-10 16:42:37 +0000

    asdjak.d

--- junjiito/src/views/contents/Hooks.jsx
+++ junjiito/src/views/contents/Hooks.jsx
@@ -1,6 +1,6 @@
 import './Hooks.css'
 import React from "react"
-import junji_ito from "https://www.google.com/url?sa=i&url=https%3A%2F%2Fultimatodobacon.com%2Fas-melhores-de-junji-ito%2F&psig=AOvVaw3akschra2-y4xMwL9IZeVT&ust=1639097329789000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCLjrm8O_1fQCFQAAAAAdAAAAABAP"
+
 
 const Hooks = props => (
     <div>
@@ -10,9 +10,8 @@ const Hooks = props => (
         </div>
         <h3>Junji Ito é um mangaká (escritor de mangás) de 58 anos. É autor de uma série de mangás que revolucionaram o mundo do terror, fugindo dos clichês já manjados por leitores assíduos do gênero.</h3>
         <h3>Suas obras não possuem o objetivo de passar medo aos leitores, mas sim de os expor em situações que geram um extremo desconforto usando a violência extrema, insetos, ou a coisa mais bizarra que você conseguir imaginar. </h3>
-        <img src={'//www.google.com/url?sa=i&url=https%3A%2F%2Fultimatodobacon.com%2Fas-melhores-de-junji-ito%2F&psig=AOvVaw3akschra2-y4xMwL9IZeVT&ust=1639097329789000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCLjrm8O_1fQCFQAAAAAdAAAAABAP'}/>
-            
-
+        
+        <p> <img scr="junji_ito.jpg"></img> </p>    
     </div>
 )
 
